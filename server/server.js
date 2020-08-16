136f28fb2e6b4742be709c5e10a99f0f3a2de0d9
Iurii <ny.iuriii@gmail.com>
2020-08-16 11:07:37 +0200

    started client side concurrently with fecking wierd syntax

--- server/server.js
+++ server/server.js
@@ -19,31 +19,90 @@ mongoose.connect(process.env.DATABASE, {
 const { User } = require("./models/user");
 const { Brand } = require("./models/brand");
 const { Wood } = require("./models/wood");
+const { Product } = require("./models/product");
 
 // Middleware
 const { auth } = require("./middleware/auth");
 const { admin } = require("./middleware/admin");
 
-//  _____________________________________WOODS
+//=================================
+//             PRODUCTS
+//=================================
+// BY ARRIVAL
+// /articles?sortBy=createdAt&order=desc&limit=4
 
-app.post('/api/product/wood',auth,admin,(req,res)=>{
-  const wood = new Wood(req.body);
+// BY SELL
+// /articles?sortBy=sold&order=desc&limit=100&skip=5
+app.get("/api/product/articles", (req, res) => {
+  let order = req.query.order ? req.query.order : "asc";
+  let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+  let limit = req.query.limit ? parseInt(req.query.limit) : 100;
 
-  wood.save((err,doc)=>{
-      if(err) return res.json({success:false,err});
-      res.status(200).json({
-          success: true,
-          wood: doc
-      })
-  })
+  Product.find()
+    .populate("brand")
+    .populate("wood")
+    .sort([[sortBy, order]])
+    .limit(limit)
+    .exec((err, articles) => {
+      if (err) return res.status(400).send(err);
+      res.send(articles);
+    });
 });
 
-app.get('/api/product/woods',(req,res)=>{
-  Wood.find({},(err,woods)=>{
-      if(err) return res.status(400).send(err);
-      res.status(200).send(woods)
-  })
-})
+app.get("/api/product/articles_by_id", (req, res) => {
+  console.log(req.query);
+  let type = req.query.type;
+  let items = req.query.id;
+
+  if (type === "array") {
+    let ids = req.query.id.split(",");
+    items = [];
+    items = ids.map((item) => {
+      return mongoose.Types.ObjectId(item);
+    });
+    // console.log(mongoose.Types.ObjectId())
+  }
+
+  Product.find({ _id: { $in: items } })
+    .populate("brand")
+    .populate("wood")
+    .exec((err, docs) => {
+      return res.status(200).send(docs);
+    });
+});
+
+app.post("/api/product/article", auth, admin, (req, res) => {
+  const product = new Product(req.body);
+
+  product.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      article: doc,
+    });
+  });
+});
+
+//  _____________________________________WOODS
+
+app.post("/api/product/wood", auth, admin, (req, res) => {
+  const wood = new Wood(req.body);
+
+  wood.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      wood: doc,
+    });
+  });
+});
+
+app.get("/api/product/woods", (req, res) => {
+  Wood.find({}, (err, woods) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).send(woods);
+  });
+});
 
 //  _____________________________________BRAND
 
