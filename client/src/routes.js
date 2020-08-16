527620103ec6b9ca75f12b35e51a3b8fd2d798cb
Iurii <ny.iuriii@gmail.com>
2020-08-16 16:32:32 +0200

    login form done before submition

--- client/src/routes.js
+++ client/src/routes.js
@@ -13,32 +13,30 @@
 
 // export default App;
 
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
 
-import Layout from './hoc/layout';
+import Layout from "./hoc/layout";
 // import Auth from './hoc/auth';
 
-// import RegisterLogin from './components/Register_login';
-// import Register from './components/Register_login/register';
+import RegisterLogin from './components/Register_login';
+// import Register from './components/Register_login';
 
 // import UserDashboard from './components/User';
 
 const Routes = () => {
-  return(
+  return (
     <Layout>
       <Switch>
         {/* <Route path="/user/dashboard" exact component={Auth(UserDashboard,true)}/>
 
-        <Route path="/register" exact component={Auth(Register,false)}/>
-        <Route path="/register_login" exact component={Auth(RegisterLogin,false)}/> */}
-        <Route path="/" exact component={Home}/>
+        <Route path="/register" exact component={Auth(Register,false)}/> */}
+        <Route path="/register_login" exact component={RegisterLogin}/>
+        <Route path="/" exact component={Home} />
       </Switch>
-     </Layout>
-
-  )
-}
+    </Layout>
+  );
+};
 
 export default Routes;
-
